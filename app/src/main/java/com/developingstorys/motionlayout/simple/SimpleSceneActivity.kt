807817ffbf170aa4127c85f0cffe051f8a3715ef
chowii <sabbib007madness@gmail.com>
2019-03-06 20:08:27 +1100

    renamed activity_scene to activity_motion; updated scene03, removed y translation from end constraint; updated colorAccent

--- app/src/main/java/com/developingstorys/motionlayout/simple/SimpleSceneActivity.kt
+++ app/src/main/java/com/developingstorys/motionlayout/simple/SimpleSceneActivity.kt
@@ -3,13 +3,13 @@ package com.developingstorys.motionlayout.simple
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.developingstorys.motionlayout.R
-import kotlinx.android.synthetic.main.activity_scene.*
+import kotlinx.android.synthetic.main.activity_motion.*
 
 class SimpleSceneActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scene)
+        setContentView(R.layout.activity_motion)
         val layoutDescriptionId = intent?.extras?.getInt(EXTRA_LAYOUT_DESCRIPTION_ID) ?: 0
         motion_layout?.loadLayoutDescription(layoutDescriptionId)
 
