73c7e889574802f2e410d9a3fb637332197d21d8
chowii <sabbib007madness@gmail.com>
2019-03-20 20:57:25 +1100

    created nultistate activity and motion scene

---
+++ app/src/main/java/com/developingstorys/motionlayout/MultiStateActivity.kt
@@ -0,0 +1,12 @@
+package com.developingstorys.motionlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MultiStateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multi_state)
+    }
+}
